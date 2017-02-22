1522fbfe06553497c105e1a9b53f3b655b46ed9f
Guure <iGuure>
2017-02-22 14:20:57 +0800

    Optimized output format

--- gendailysentence.py
+++ gendailysentence.py
@@ -10,7 +10,9 @@ json_data = open(json_file)
 data = json.load(json_data)
 json_data.close()
 
-print('\n\n\n' + data[0]['quote'].center(120) + '\n\n' + '--'.rjust(100) + data[0]['author'])
+author = '--' + data[0]['author']
+
+print('\n\n\n' + data[0]['quote'].center(120) + '\n\n' + author.rjust(100))
 
 os.system("del item.json")
 
